f80bfd23ff9d0f5a0de9070724f4d0a6b82451bc
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-08-13 05:59:03 -0400

    Se modifica componente de inputs

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,28 +1,11 @@
-import { React, useState } from 'react';
+import React from 'react';
 import NumberInput from './NumberInput';
-import ComponenteEjemplo from './componente-ejemplo'
-import Resultado from './Resultado';
 
 const Calculator = () => {
-    const [suma, setsuma] = useState(50);
 
    return (
        <div>
-            <NumberInput name='Numero 1'/>
-            <NumberInput name='Numero 2'/>
-            <br />
-           
-           <Resultado operacion='Suma' calculo={suma}/>
-           <Resultado operacion='Resta' calculo={suma}/>
-           <Resultado operacion='Multiplicacion' calculo={suma}/>
-           <Resultado operacion='Division' calculo={suma}/>
-           
-           <span>Resta: {suma}</span>
-           <br />
-           <span>Multiplicacion: {suma}</span>
-           <br />
-           <span>Division: {suma}</span>
-
+            <NumberInput />
        </div>
    ) 
 }
--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import { React, useState} from 'react'
 import PropTypes from 'prop-types'
+import Resultado from './Resultado';
 
-const NumberInput = ({name}) => {
+
+const NumberInput = () => {
+    
+    const [numeros, setNumeros] = useState({
+        numero1: 10,
+        numero2: 5
+    });
+    
+    const {numero1, numero2} = numeros;
+    const [suma, setsuma] = useState(numero1 + numero2);
+
+    const handleChange = (e)=>{
+        
+        setNumeros({
+            ...numeros,
+            [e.target.name]: parseFloat(e.target.value),
+        });
+        
+        setsuma((actual) => actual + [e.target.value]);
+    }
+
     return (
         <div>
             <label>
-                {name}: <input type="text" name="" id="" />
+                Numero 1: <input name='numero1' value={numero1} type="number" onChange={handleChange} />
             </label>
+            <label>
+                Numero 2: <input name='numero2' value={numero2} type="number" onChange={handleChange} />
+            </label>
+
+            <br />
+           
+           <Resultado operacion='Suma' calculo={suma}/>
+           <Resultado operacion='Resta' calculo={suma}/>
+           <Resultado operacion='Multiplicacion' calculo={suma}/>
+           <Resultado operacion='Division' calculo={suma}/>
+                   
         </div>
     )
 }
